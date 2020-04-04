ae7b24d1c69bb6015323ff589902c2c2aa5c4905
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-04-04 10:23:30 -0700

    Remove texel per unit forcing.

--- merge.cpp
+++ merge.cpp
@@ -504,7 +504,6 @@ void MeshMergeMaterialRepack::_generate_atlas(const int32_t p_num_meshes, Vector
 			mesh_count++;
 		}
 	}
-	pack_options.texelsPerUnit = 1.0f;
 	pack_options.maxChartSize = 4096;
 	pack_options.blockAlign = true;
 	xatlas::PackCharts(atlas, pack_options);
