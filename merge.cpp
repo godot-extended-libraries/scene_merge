eaf0d87675372a139f98c54d2a0f52b55768b46c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-04-12 13:49:01 -0700

    Reduce padding to avoid exceeding datastructure limits.

--- merge.cpp
+++ merge.cpp
@@ -660,7 +660,7 @@ void MeshMergeMaterialRepack::_generate_atlas(const int32_t p_num_meshes, Vector
 			mesh_count++;
 		}
 	}
-	pack_options.padding = 32.0f;
+	pack_options.padding = 16.0f;
 	pack_options.texelsPerUnit = 1.0f;
 	pack_options.maxChartSize = 4096;
 	pack_options.blockAlign = true;
