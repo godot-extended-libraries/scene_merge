4db2bfab585dda1ffa8c4d56e7017a5f11f32f75
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-04-12 12:43:54 -0700

    Remove padding.

--- merge.cpp
+++ merge.cpp
@@ -661,7 +661,6 @@ void MeshMergeMaterialRepack::_generate_atlas(const int32_t p_num_meshes, Vector
 			mesh_count++;
 		}
 	}
-	pack_options.padding = 32;
 	pack_options.texelsPerUnit = 1.0f;
 	pack_options.maxChartSize = 4096;
 	pack_options.blockAlign = true;
